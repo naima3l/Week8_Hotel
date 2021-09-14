fe1d8bd3c967e261259b8b28de2bfef00f2243de
naima_3l <45000187+naima3l@users.noreply.github.com>
2021-09-14 16:09:16 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+﻿function homeOnLoad() {
+    alert("Welcome!");
+}
+
+function formAppear() {
+    let form = document.getElementById("formLogin");
+    let button = document.getElementById("login");
+
+    form.style.visibility = "visible";
+    button.style.visibility = "";
+}
